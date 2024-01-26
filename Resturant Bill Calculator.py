2f1dbfca5feac9df5685f8bb92b634e9261e69ce
Farah Maqbool <151655153+Farah-Maqbool@users.noreply.github.com>
2024-01-26 21:36:38 +0500

    Update Resturant Bill Calculator.py

--- Resturant Bill Calculator.py
+++ Resturant Bill Calculator.py
@@ -3,57 +3,45 @@
 Item={"Biryani":200,"Pizza":600,"Burger":100,"Fries":50,"Cake":500,"Chiken Seekh kabab":600,"Desi Chowmein":1000,"Chiken Tikka karahi":700}
 for key, values in Item.items():
     print(key,values)
-def bill(enter_item,enter_quantity,enter_discount,tax_rate,people):
+def bill(your_item,enter_discount,tax_rate,people):
+
     #cost
-    cost=1
-    for key, values in Item.items():
-        if key==enter_item:
-            cost=enter_quantity*values
-    print("Cost is",cost)
+    total_cost=0
+    for key1, values1 in your_item.items():
+        for key2, values2 in Item.items():
+            if key1==key2:
+                cost=values1*values2
+                total_cost+=cost
+
+    print("Cost is",total_cost)
     #discount calculation
     discount_decimal=enter_discount/100
-    discount_point=cost*discount_decimal
-    value_after_discount=cost-discount_point
+    discount_point=total_cost*discount_decimal
+    value_after_discount=total_cost-discount_point
     discount=value_after_discount
-    print("The Amount after the discount=",discount)
+    print("The Amount after the discount is=",discount)
     #tax Calculation
     tax_decimal=tax_rate/100
-    tax_point=cost*tax_decimal
+    tax_point=total_cost*tax_decimal
     value_after_tax=discount+tax_point
     amount=value_after_tax
-    print("The amount after cutting tax=",value_after_tax)
+    print("The amount after tax is=",value_after_tax)
     # how many amount paid by 1 person
     one_person=int(amount/people)
     print("Total Amount",amount)
     print("One Person will pay the",one_person,"Rupees")
 
+your_item={}
+print('If you want to stop enter "Stop"')
+while True:
 
-
-
-enter_item=input("Enter Item: ")
-enter_quantity=int(input("Enter Quantity: "))
+    enter_item=input("Enter Item: ")
+    enter_quantity=input("Enter Quantity: ")
+    if enter_item=="stop" and enter_quantity=="stop":
+        break
+    else:
+        your_item[enter_item]=int(enter_quantity)
 enter_discount=int(input("Enter Discount: "))
 tax_rate=int(input("Enter Tax Rate: "))
 people=int(input("Number of People: "))
-bill(enter_item,enter_quantity,enter_discount,tax_rate,people)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+bill(your_item,enter_discount,tax_rate,people)
